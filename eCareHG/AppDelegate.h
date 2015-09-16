3129cd99d693fb4e57357e18e25d44a606f8c321
JinYanFeng <jinyanfeng@JinYanFengdeMacBook-Pro.local>
2015-09-16 14:37:57 +0800

    Initial Commit

---
+++ eCareHG/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  eCareHG
+//
+//  Created by JinYanFeng on 15/9/16.
+//  Copyright (c) 2015年 JinYanFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ eCareHG/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  eCareHG
+//
+//  Created by JinYanFeng on 15/9/16.
+//  Copyright (c) 2015年 JinYanFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
